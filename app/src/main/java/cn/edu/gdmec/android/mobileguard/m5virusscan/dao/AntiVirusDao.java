1d29546d46fd18766231d623d6cdd63fc8b8555c
DaoSuper <3092058517@qq.com>
2017-12-21 15:48:09 +0800

    12.20 bug调试

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/dao/AntiVirusDao.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/dao/AntiVirusDao.java
@@ -10,31 +10,40 @@ import android.database.sqlite.SQLiteDatabase;
 
 public class AntiVirusDao {
     /**
-     * 检查某个md5是否有病毒
+     * 检查某个md5是否是病毒
      * @param md5
      * @return null 代表扫描安全
      */
     private static Context context;
     private static String dbname;
     public AntiVirusDao(Context context){
-        this.context = context;
-        dbname = "/data/data/" + context.getPackageName() + "/files/antivirus.db";
+        this.context =context;
+        dbname ="/data/data/"+context.getPackageName()+"/files/antivirus.db";
     }
-    //使用apk文件的md5值得配对病毒数据库
-    public String checkVirus(String md5){
+
+    /**
+     * 使用apk文件的md5值匹配病毒数据库
+     */
+
+    public String checkVirus(String md5) {
         String desc = null;
-        //打开数据库
-        SQLiteDatabase db = SQLiteDatabase.openDatabase(dbname,null,SQLiteDatabase.OPEN_READONLY);
-        Cursor cursor = db.rawQuery("select desc from datable where md5=?",new String[] { md5 });
-        if(cursor.moveToNext()){
+        // 打开病毒数据库
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(
+                dbname, null,
+                SQLiteDatabase.OPEN_READONLY);
+        Cursor cursor = db.rawQuery("select desc from datable where md5=?",
+                new String[] { md5 });
+        if (cursor.moveToNext()) {
             desc = cursor.getString(0);
         }
         cursor.close();
         db.close();
         return desc;
     }
+    /**
+     * 获取病毒数据版本
+     */
 
-    //获取病毒数据版本
     public String getVirusDbVersion(){
         String dbVersion = null;
         // 打开病毒数据库
